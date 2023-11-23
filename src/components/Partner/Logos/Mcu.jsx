71e92a41ea517aaf04868405f5ccb384daaad7f0
Diego Souza <disouzauva@gmail.com>
2023-11-23 12:46:51 -0300

    fix: corrigindo problemas na responsividade dos componentes da home

--- src/components/Partner/Logos/Mcu.jsx
+++ src/components/Partner/Logos/Mcu.jsx
@@ -3,20 +3,8 @@ import React from "react";
 
 export default function Mcu({ theme }) {
   if (theme === "dark") {
-    return (
-      <StaticImage
-        placeholder="none"
-        src="../../../assets/img/partners/parceiro-mcu-white.png"
-        alt="Fundação Planetário do Rio de Janeiro"
-      />
-    );
+    return <StaticImage placeholder="none" width={150} height={56} src="../../../assets/img/partners/parceiro-mcu-white.png" alt="Fundação Planetário do Rio de Janeiro"/>;
   }
 
-  return (
-    <StaticImage
-      placeholder="none"
-      src="../../../assets/img/partners/parceiro-mcu.png"
-      alt="Fundação Planetário do Rio de Janeiro"
-    />
-  );
+  return <StaticImage placeholder="none" width='150' height='56' src="../../../assets/img/partners/parceiro-mcu.png" alt="Fundação Planetário do Rio de Janeiro"/>;
 }
