3a331d370c5642b85a4eb036dd5c6059626a8fb3
Diego Souza <disouzauva@gmail.com>
2023-11-23 17:41:18 -0300

    feat: Implementando evento de touch no slide de depoimentos

--- src/components/Testemonials/Testemonials.jsx
+++ src/components/Testemonials/Testemonials.jsx
@@ -6,6 +6,9 @@ import CardTestemonial from "./CardTestemonial/CardTestemonial";
 
 export default function Testemonials() {
   const [itemActive, setItemActive] = React.useState(0);
+  const [initSliderDrag, setInitSliderDrag] = React.useState(false);
+  const [sliderPosiion, setSliderPosiion] = React.useState({startX: 0, movement: 0, finalX:0});
+
   const { testemonials } = useStaticQuery(query).dataJson;
 
   function handleNextItem(){
@@ -16,6 +19,29 @@ export default function Testemonials() {
     setItemActive((prev) => prev - 1 < 0 ? testemonials.length - 1 : prev - 1);
   }
 
+  function initDragSlider(event){
+    setInitSliderDrag(true);
+    setSliderPosiion((prev) => {return {...prev, startX: event.clientX, finalX: 0}});
+  }
+
+  function handleMouseMove(event){
+    if(!initSliderDrag) return;
+    setSliderPosiion((prev) => {return {...prev, movement: sliderPosiion.startX - event.clientX}});
+  }
+
+  function endDragSlider(){
+    setInitSliderDrag(false);
+    setSliderPosiion((prev) => {return {...prev, finalX: sliderPosiion.movement}});
+  }
+
+  React.useEffect(() => {
+    if(!initSliderDrag && sliderPosiion.finalX !== 0) {
+      sliderPosiion.finalX > 0 ? handleNextItem() : handlePrevItem();
+    };   
+
+
+  }, [initSliderDrag]);
+
   return (
     <section className={styles.section} aria-label="Depoimentos">
       <div className={`${styles.container} container`}>
@@ -23,7 +49,7 @@ export default function Testemonials() {
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque, impedit.</p>
 
         <div className={styles.wrapper}>
-          <ul className={styles.slider}>
+          <ul className={`${styles.slider} ${initSliderDrag ? styles.drag : ''}`} onMouseDown={initDragSlider} onMouseMove={handleMouseMove} onMouseLeave={endDragSlider} onMouseUp={endDragSlider}>
             {testemonials.map((testemonial, index) => <li className={index === itemActive ? styles.active : ''} key={testemonial.name} style={{transform: `translate3D(-${100 * itemActive}%, 0, 0)`}} aria-hidden={index !== itemActive ? true : false}><CardTestemonial info={testemonial} /></li>)}
           </ul>
 
